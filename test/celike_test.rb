2fad277a7e77f91b09b4920ff9cc5942888fcace
Gunter Miegel <gunter.miegel@startnext.com>
2020-02-26 11:16:50 +0100

    Adapt test reflect change of options

--- test/celike_test.rb
+++ test/celike_test.rb
@@ -3,15 +3,21 @@ describe command('celkie backup') do
   its('stdout') { should match '.*completed OK!' }
 end
 
+sleep(5)
+
 describe command('celkie list') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match '.*_full' }
 end
 
-describe command('celkie backup --database sakila') do
+describe command('celkie backup --database=sakila') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match '.*completed OK!' }
 end
+
+
+sleep(5)
+
 
 get_last_full_backup = command("celkie list | grep -oP '.*[0-9]{6}_full$' | tail -1" )
 
@@ -20,7 +26,7 @@ describe get_last_full_backup do
   its('stdout') { should match '.*[0-9]{6}_full$' }
 end
 
-describe command('celkie dump --backup ' + get_last_full_backup.stdout.strip) do
+describe command('celkie dump --backup=' + get_last_full_backup.stdout.strip) do
   its('exit_status') { should eq 0 }
   its('stdout') { should match '.*completed OK!' }
   its('stdout') { should match '.*Database dump created.*' }
@@ -30,7 +36,7 @@ describe docker_container('celkie-mariadb') do
   it { should_not exist }
 end
 
-describe command('celkie dump --backup ' + get_last_full_backup.stdout.strip + ' --database sakila') do
+describe command('celkie dump --backup=' + get_last_full_backup.stdout.strip + ' --database=sakila') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match '.*completed OK!' }
   its('stdout') { should match '.*Database dump created.*' }
@@ -40,7 +46,7 @@ describe docker_container('celkie-mariadb') do
   it { should_not exist }
 end
 
-describe command('celkie dump --backup ' + get_last_full_backup.stdout.strip + ' --database sakila' + ' --table film actor city') do
+describe command('celkie dump --backup ' + get_last_full_backup.stdout.strip + ' --database=sakila' + ' --table=film,actor,city') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match '.*completed OK!' }
   its('stdout') { should match '.*Database dump created.*' }
